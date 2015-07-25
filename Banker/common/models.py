4e05e6aadeb5242b2798b4b8f8c1dbece4d7aca9
Christopher Henderson <chris@desktopVM.chenderson.org>
2015-07-25 15:43:45 -0700

    First drop of Banker

---
+++ Banker/common/models.py
@@ -0,0 +1,26 @@
+from django.db.models import Model
+from django.contrib.auth.models import User
+
+class Account(Model):
+    pass
+
+class Classification(Model):
+    pass
+
+class Transaction(Model):
+    pass
+
+class Time(Model):
+    pass
+
+class Bill(Model):
+    pass
+
+class Vendor(Model):
+    pass
+
+class Item(Model):
+    pass
+
+class Statistic(Model):
+    pass
---
+++ Banker/common/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework.serializers import Serializer
+
+class AccountSerializer(Serializer):
+    pass
+
+class ClassificationSerializer(Serializer):
+    pass
+
+class TransactionSerializer(Serializer):
+    pass
+
+class TimeSerializer(Serializer):
+    pass
+
+class BillSerializer(Serializer):
+    pass
+
+class VendorSerializer(Serializer):
+    pass
+
+class ItemSerializer(Serializer):
+    pass
+
+class StatisticSerializer(Serializer):
+    pass
