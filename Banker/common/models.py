eef82ba5ac387fd835c4b47a6fa8c9da553d871b
christopher-henderson <chris@chenderson.org>
2015-08-08 09:36:56 -0700

    Continued work on models

--- Banker/common/models.py
+++ Banker/common/models.py
@@ -1,26 +1,43 @@
-from django.db.models import Model
+from django.db.models import *
 from django.contrib.auth.models import User
 
+
 class Account(Model):
-    pass
+    user = ForeignKey(User)
+
+
+class Transaction(Model):
+    account = ForeignKey(Account)
+    vendor = ForeignKey(Vendor)
+    bill = ForeignKey(Bill)
+    items = ManyToMany(Item)
+
+
+class Vendor(Model):
+    name = CharField(max_length=4096)
+    location = CharField(max_length=4096)
+
+
+class Item(Model):
+    name = CharField(max_length=4096)
+    classification = ForeignKey(Classification)
+
+
+class Time(Model):
+    time = DateTime()
+
 
 class Classification(Model):
     pass
 
-class Transaction(Model):
-    pass
 
-class Time(Model):
-    pass
+class Price(Model):
+    items = ManyToMany(Item)
+
 
 class Bill(Model):
-    pass
+    amount = DecimalField(max_digits=10, decimal_places=2)
 
-class Vendor(Model):
-    pass
-
-class Item(Model):
-    pass
 
 class Statistic(Model):
     pass
